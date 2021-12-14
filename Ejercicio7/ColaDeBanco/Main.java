b5ba6ab9537eab69051e162ec472b2b1aa81d419
CinSab <73029945+CinSab@users.noreply.github.com>
2021-12-14 13:41:29 -0300

    Update Main.java

--- Ejercicio7/ColaDeBanco/Main.java
+++ Ejercicio7/ColaDeBanco/Main.java
@@ -2,11 +2,8 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args){
-        Random numGenerator = new Random();
-        numGenerator.ints();
-        int Num = (int)(Math.random() * 100);
         ColaDeBanco colaDeBanco = new ColaDeBanco();
-        colaDeBanco.generadorDePersonas(Num);
+        colaDeBanco.generadorDePersonas(5);
         System.out.println(colaDeBanco.colaDePersonas);
         System.out.println(colaDeBanco.atender());
         System.out.println(colaDeBanco.colaDePersonas);
