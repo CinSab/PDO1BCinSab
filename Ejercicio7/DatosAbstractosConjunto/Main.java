5fba470b0b091183f34b7f3296b2ddfcd7a7edef
CinSab <73029945+CinSab@users.noreply.github.com>
2021-12-15 20:10:39 -0300

    it works

--- Ejercicio7/DatosAbstractosConjunto/Main.java
+++ Ejercicio7/DatosAbstractosConjunto/Main.java
@@ -1,19 +1,24 @@
 import java.util.List;
 
 public class Main {
-    public void main(String[] args){
+    public static void main(String[] args){
         Par numm = new Par(2);
         Par num = new Par(1);
-        Par nopar = new Par(7);
+        Integer nopar = 7;
+        Integer par = 2;
+        Long doble = 1233l;
         Lista lista = new Lista();
         lista.AgregarObjLista(numm);
         lista.AgregarObjLista(num);
 
         System.out.println(lista.pertenece(num));
         System.out.println(lista.pertenece(numm));
+        System.out.println(lista.pertenece(doble));
         System.out.println(lista.pertenece(nopar));
-        System.out.println(num.pertenece(num));
-        System.out.println(numm.pertenece(numm));
+        System.out.println(num.pertenece(nopar));
+        System.out.println(numm.pertenece(par));
+        System.out.println(numm.pertenece(nopar));
 
     }
 }
+
